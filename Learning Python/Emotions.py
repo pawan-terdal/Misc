0f7e2c1ea5abdee68778fad9de2688ad2764f201
rednithin <reddy.nithinpg@live.com>
2016-10-19 01:03:32 +0530

    LOL

---
+++ Learning Python/Emotions.py
@@ -0,0 +1,94 @@
+import pyperclip
+import time 
+import requests
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+_url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+_key = '89a21fa608744bf4b108017cdf20eb62'
+_maxNumRetries = 10
+
+
+def processRequest( json, data, headers, params ):
+
+    """
+    Helper function to process the request to Project Oxford
+
+    Parameters:
+    json: Used when processing images from its URL. See API Documentation
+    data: Used when processing image read from disk. See API Documentation
+    headers: Used to pass the key information and the data type request
+    """
+
+    retries = 0
+    result = None
+
+    while True:
+
+        response = requests.request( 'post', _url, json = json, data = data, headers = headers, params = params )
+
+        if response.status_code == 429: 
+
+            print( "Message: %s" % ( response.json()['error']['message'] ) )
+
+            if retries <= _maxNumRetries: 
+                time.sleep(1) 
+                retries += 1
+                continue
+            else: 
+                print( 'Error: failed after retrying!' )
+                break
+
+        elif response.status_code == 200 or response.status_code == 201:
+
+            if 'content-length' in response.headers and int(response.headers['content-length']) == 0: 
+                result = None 
+            elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str): 
+                if 'application/json' in response.headers['content-type'].lower(): 
+                    result = response.json() if response.content else None 
+                elif 'image' in response.headers['content-type'].lower(): 
+                    result = response.content
+        else:
+            print( "Error code: %d" % ( response.status_code ) )
+            print( "Message: %s" % ( response.json()['error']['message'] ) )
+
+        break
+        
+    return result
+
+"""
+urlImage = pyperclip.paste()
+
+headers = dict()
+headers['Ocp-Apim-Subscription-Key'] = _key
+headers['Content-Type'] = 'application/json' 
+
+json = { 'url': urlImage } 
+data = None
+params = None
+
+result = processRequest( json, data, headers, params )
+
+print(result)
+"""
+
+pathToFileInDisk = r'./smiling-photo.jpg'
+with open( pathToFileInDisk, 'rb' ) as f:
+    data = f.read()
+
+headers = dict()
+headers['Ocp-Apim-Subscription-Key'] = _key
+headers['Content-Type'] = 'application/octet-stream'
+
+json = None
+params = None
+result = processRequest( json, data, headers, params )
+keys = result[0]['scores'].keys()
+markings = list(range(1,9))
+values = [5,4,3,21,41,65,76,56]
+plt.scatter(markings,values)
+plt.xticks(markings,keys)
+
+plt.show()
---
+++ Learning Python/Numpy.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def __name__():
+    npArr = np.array([[1, 2], [3, 4]])
+    print(type(npArr))
