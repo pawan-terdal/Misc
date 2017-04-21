6ed5f1e16477905ebaec3fbd1fa46d4e931d8cf7
rednithin <reddy.nithinpg@live.com>
2017-04-21 17:00:52 +0530

    Script Commit

---
+++ Lab Programs/Fourth Sem/Prog11.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+class Prog11
+{
+	public static int GivenSum = 0;
+	public static int NoOfSolutions = 0;
+	public static int Sum(int arr[], int position)
+	{
+		int sum = 0;
+		for(int i = 0; i <= position; i++)
+		{	
+			sum += arr[i];
+		}
+		return sum;
+	}
+	public static void Combinations(int offset, int position, int elements[], int array[])
+	{
+		if(position == array.length)
+		{
+			return;
+		}
+		for(int i = offset; i < elements.length; i++)
+		{
+			array[position] = elements[i];
+			Combinations(i + 1, position + 1, elements, array);
+			if(Sum(array, position) > GivenSum)
+			{
+				return;
+			}
+			if(Sum(array, position) == GivenSum)
+			{
+				NoOfSolutions++;
+				System.out.print("[ ");
+				for(int j = 0; j <= position; j++)
+				{
+					System.out.print(array[j] + " ");
+				}
+				System.out.println("]");
+			}
+		}
+	}
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter number of elements : ");
+		int n = in.nextInt();
+		System.out.println("Enter elements : ");
+		int elements[] = new int[n];
+		int array[] = new int[n];
+		for(int i = 0; i < n; i++)
+		{
+			elements[i] = in.nextInt();
+		}
+		System.out.println("Enter Subset Sum : ");
+		GivenSum = in.nextInt();
+		Combinations(0, 0, elements, array);
+		if(NoOfSolutions == 0)
+		{
+			System.out.println("No Subsets Satisfying Conditions.");
+		}
+	}
+}
+/*
+
+Enter number of elements : 
+5
+Enter elements : 
+1 2 5 6 8
+Enter Subset Sum : 
+16
+[ 1 2 5 8 ]
+[ 2 6 8 ]
+
+
+*/
---
+++ Lab Programs/Fourth Sem/Prog12.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+class Prog12
+{
+	public static boolean Hamilton(int vertex, int matrix[][], int visited[], Stack stack, int count, int vertices)
+	{
+		visited[vertex]++;
+		count++;
+		stack.Push(new Integer(vertex));
+		if(count == vertices)
+		{
+			if(matrix[vertex][0] != -1)
+			{
+				System.out.println("The Hamilton path is : ");
+				for(int i = 0; i < count; i++)
+				{
+					System.out.print((char)(65 + (int) stack.Pop()) + ", ");
+				}
+				System.out.println((char)(65 + vertex));
+				return true;
+			}
+			return false;
+		}
+
+		for(int j = 0; j < vertices; j++)
+		{
+			if(j == vertex)
+			{
+				continue;
+			}
+			if(matrix[vertex][j] != -1 && visited[j] == 0)
+			{
+				if(Hamilton(j, matrix, visited, stack, count, vertices))
+				{
+					return true;
+				}
+				visited[j] = 0;
+				stack.Pop();
+			}
+		}
+		return false;
+	}
+
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter number of vertices : ");
+		int vertices = in.nextInt();
+		int matrix[][] = new int[vertices][vertices];
+		System.out.println("Enter NxN adjacency matrix : ");
+		for(int i = 0; i < vertices; i++)
+		{
+			for(int j = 0; j < vertices; j++)
+			{
+				matrix[i][j] = in.nextInt();
+			}
+		}
+		int visited[] = new int[vertices];
+		int count = 0;
+		Stack stack = new Stack();
+		if(! Hamilton(0, matrix, visited, stack, count, vertices))
+		{
+			System.out.println("No Hamilton Circuit.");
+		}
+	}
+}
+/*
+0 1 -1 1 1
+1 0 1 -1 -1
+-1 1 0 -1 1
+1 -1 -1 0 1
+1 -1 1 1 0
+
+0 1 -1 -1 1
+1 0 1 -1 -1
+-1 1 0 -1 1
+-1 -1 -1 0 1
+1 -1 1 1 0
+*/
